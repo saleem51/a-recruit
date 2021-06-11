be6ea4baa406ec834263e6a80190b62171adbf67
saleem51 <saleem51@hotmail.fr>
2021-06-11 11:48:21 +0200

    bootstrap module

---
+++ pages/inscription.js
@@ -0,0 +1,18 @@
+import styles from '../styles/Home.module.css';
+import Head from 'next/head';
+
+export default function SignIn() {
+    return (
+        <div className="SignDiv">
+            <Head>
+                <title>a-recruit</title>
+            </Head>
+
+            <main>
+                <div>
+                    <p>Le formulaire d'inscription</p>
+                </div>
+            </main>
+        </div>
+    )
+}
